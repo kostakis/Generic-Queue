8a7c82c7015f5247234a8f5c0894ca21c6c96a26
Akis Kostakis <akiskost97@outlook.com>
2019-12-01 22:36:27 +0200

    Main file to test the queue

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#pragma once
 #include "queue.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,6 +11,14 @@ struct temp {
 
 int main() {
 	queue* q = createQueue(sizeof(struct temp));
+	struct temp t;
+	t.a = 10;
+	enqueue(q, &t);
+
+	struct temp test;
+	front(q,&test);
+	printf("Front of the Q is %d\n",test.a);
+
 	for (int i = 0; i < 10; i++) {
 		struct temp a; 
 		a.c = i+10;
@@ -20,6 +27,7 @@ int main() {
 		enqueue(q, &a);
 	}
 	printf("Size of queue is %ld\n",getSize(q));
+	
 	while (!isEmpty(q)) {
 		struct temp temp;
 		dequeue(q, &temp);
@@ -27,8 +35,8 @@ int main() {
 	}
 	clearQueue(q);
 	printf("Cleared queue\n");
-	printf("Size of queue is %ld\n",getSize(q));
 	destroyQueue(q);
 	printf("Destroyed queue\n");
+
 	return 0;
 }
