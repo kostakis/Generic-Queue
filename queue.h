c01a9edb15c8c2552a625822f971625421519408
Akis Kostakis <akiskost97@outlook.com>
2022-01-23 19:55:00 +0200

    C++ Support

--- queue.h
+++ queue.h
@@ -5,71 +5,79 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct _data {
-	void* data;
-	struct _data* next;
-}data;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef struct _queue {
-	size_t size;
-	size_t allocationSize;
-	data* head;
-	data* tail;
-}queue;
+	typedef struct _data {
+		void* data;
+		struct _data* next;
+	}data;
 
-/**
- * @param allocSize Size of elements insrted
- * @return queue on success, NULL on error
- */
-queue* createQueue(size_t allocSize);
+	typedef struct _queue {
+		size_t size;
+		size_t allocationSize;
+		data* head;
+		data* tail;
+	}queue;
 
-/**
- * @param q The queue
- * @param data Data to be inserted
- * @return queue on success, NULL on error
- */
-queue* enqueue(queue* q, void* data);
+	/**
+	 * @param allocSize Size of elements insrted
+	 * @return queue on success, NULL on error
+	 */
+	queue* createQueue(size_t allocSize);
 
-/**
- * @param q The queue
- * @param deletedElem The element deleted
- * @return queue on success, NULL on error
- */
-queue* dequeue(queue* q, void* toRet);
+	/**
+	 * @param q The queue
+	 * @param data Data to be inserted
+	 * @return queue on success, NULL on error
+	 */
+	queue* enqueue(queue* q, void* data);
 
-/**
- * @brief Get the front element
- * @param q The queue
- * @param toRet The first element
- * @return queue on success, NULL on error
- */
-queue* front(queue* q, void* toRet);
+	/**
+	 * @param q The queue
+	 * @param deletedElem The element deleted
+	 * @return queue on success, NULL on error
+	 */
+	queue* dequeue(queue* q, void* toRet);
 
-/**
- * @brief Delete all the elements of the queue
- * @param q The queue
- * @return queue on success, NULL on error
- */
-queue* clearQueue(queue* q);
+	/**
+	 * @brief Get the front element
+	 * @param q The queue
+	 * @param toRet The first element
+	 * @return queue on success, NULL on error
+	 */
+	queue* front(queue* q, void* toRet);
 
-/**
- * @brief Destroy the queue
- * @param q The queue
- */
-void destroyQueue(queue* q);
+	/**
+	 * @brief Delete all the elements of the queue
+	 * @param q The queue
+	 * @return queue on success, NULL on error
+	 */
+	queue* clearQueue(queue* q);
 
-/**
- * @param q The queue
- * @return size_t Size of queue
- */
-size_t getSize(queue* q);
+	/**
+	 * @brief Destroy the queue
+	 * @param q The queue
+	 */
+	void destroyQueue(queue* q);
 
-/**
- * @brief Check if queue is empty
- *
- * @param q The queue
- * @return true if size >= 1, false otherwise,
- */
-bool isEmpty(queue* q);
+	/**
+	 * @param q The queue
+	 * @return size_t Size of queue
+	 */
+	size_t getSize(queue* q);
+
+	/**
+	 * @brief Check if queue is empty
+	 *
+	 * @param q The queue
+	 * @return true if size >= 1, false otherwise,
+	 */
+	bool isEmpty(queue* q);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
